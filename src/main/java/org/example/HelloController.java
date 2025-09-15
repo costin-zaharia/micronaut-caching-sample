27b04be7ac8e950fbdb764eed969b90623c2e47c
Costin Zaharia <cgzaharia@gmail.com>
2025-09-15 21:27:53 +0200

    News controller

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -4,11 +4,26 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import java.util.List;
+
 @Controller("/hello")
 public class HelloController {
+
+    private final NewsService newsService;
+
+    public HelloController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
         return "Hello World";
     }
+
+    @Get(produces = MediaType.APPLICATION_JSON, value = "/news")
+    public List<String> news() {
+        return newsService.getNews("category");
+    }
+
 }
